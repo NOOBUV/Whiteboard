9dd8edc07b4a93ae1c422d3d0c6003a22db4c407
Zaheer Abbas <nk4456542@gmail.com>
2021-07-04 08:51:23 +0530

    HTML for topbar

--- index.js
+++ index.js
@@ -1,112 +1,79 @@
-window.addEventListener("load", () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+// window.addEventListener("load", () => {
+//   const canvas = document.getElementById("canvas");
+//   const ctx = canvas.getContext("2d");
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
 
-  let painting = false;
-  var colorCount = 0;
-  var bgCount = 0;
-  let size = 2;
-  let mode = true;
+//   console.log(canvas.width);
+//   console.log(canvas.height);
 
-  function startPainting(event) {
-    painting = true;
-    makeIt(event);
-  }
+//   let painting = false;
 
-  function endPainting() {
-    painting = false;
-    ctx.beginPath();
-  }
+//   function startPosition(e) {
+//     painting = true;
+//     draw(e);
+//   }// window.addEventListener("load", () => {
+//   const canvas = document.getElementById("canvas");
+//   const ctx = canvas.getContext("2d");
 
-  function increaseSize() {
-    size += 1;
-    document.getElementById("pen-size").innerText = size;
-  }
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
 
-  function decreaseSize() {
-    if (size > 1) {
-      size -= 1;
-      document.getElementById("pen-size").innerText = size;
-    }
-  }
+//   console.log(canvas.width);
+//   console.log(canvas.height);
 
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+//   let painting = false;
 
-  function makeIt(event) {
-    console.log("cxas");
-    if (mode) {
-      draw(event);
-    } else {
-      erase(event);
-    }
-  }
+//   function startPosition(e) {
+//     painting = true;
+//     draw(e);
+//   }
 
-  function draw(event) {
-    if (!painting) {
-      return;
-    }
+//   function finishedPosition() {
+//     painting = false;
+//     ctx.beginPath();
+//   }
 
-    ctx.lineWidth = size;
-    ctx.lineCap = "round";
+//   function draw(e) {
+//     if (!painting) {
+//       return;
+//     }
 
-    ctx.lineTo(event.clientX, event.clientY);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(event.clientX, event.clientY);
-  }
+//     ctx.lineWidth = 10;
 
-  function erase(event) {
-    if (!painting) {
-      return;
-    }
+//     ctx.lineCap = "round";
+//     ctx.lineTo(e.clientX, e.clientY);
+//     ctx.stroke();
+//     ctx.beginPath();
+//     ctx.moveTo(e.clientX, e.clientY);
+//   }
 
-    ctx.lineWidth = size;
-    ctx.lineCap = "round";
+//   canvas.addEventListener("mousedown", startPosition);
+//   canvas.addEventListener("mouseup", finishedPosition);
+//   canvas.addEventListener("mousemove", draw);
+// });
 
-    ctx.clearRect(event.clientX - 2, event.clientY - 2, 30, 30);
-  }
+//   function finishedPosition() {
+//     painting = false;
+//     ctx.beginPath();
+//   }
 
-  function cleanScreen() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+//   function draw(e) {
+//     if (!painting) {
+//       return;
+//     }
 
-  function changeBgColor() {
-    var colors = ["#98ddca", "#126e82", "#ffc93c", "#fea82f", "white"];
-    const col = colors[bgCount % 5];
-    bgCount += 1;
-    document.getElementById("bg-color").style.backgroundColor = col;
-    canvas.style.backgroundColor = col;
-  }
+//     ctx.lineWidth = 10;
 
-  function changePenColor() {
-    var colors = ["black", "red", "blue", "#fea82f"];
-    const col = colors[colorCount % 4];
-    colorCount += 1;
-    document.getElementById("pen-color").style.color = col;
-    ctx.strokeStyle = col;
-  }
+//     ctx.lineCap = "round";
+//     ctx.lineTo(e.clientX, e.clientY);
+//     ctx.stroke();
+//     ctx.beginPath();
+//     ctx.moveTo(e.clientX, e.clientY);
+//   }
 
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", endPainting);
-  canvas.addEventListener("mousemove", makeIt);
-  canvas.addEventListener("resize", resizeCanvas);
-  document.getElementById("pen-color").addEventListener("click", () => {
-    mode = true;
-  });
-  document.getElementById("eraser").addEventListener("click", () => {
-    mode = false;
-  });
-  document.getElementById("clean").addEventListener("click", cleanScreen);
-  document.getElementById("bg-color").addEventListener("click", changeBgColor);
-  document
-    .getElementById("pen-color")
-    .addEventListener("click", changePenColor);
-  document.getElementById("pen-up").addEventListener("click", increaseSize);
-  document.getElementById("pen-down").addEventListener("click", decreaseSize);
-});
+//   canvas.addEventListener("mousedown", startPosition);
+//   canvas.addEventListener("mouseup", finishedPosition);
+//   canvas.addEventListener("mousemove", draw);
+// });
